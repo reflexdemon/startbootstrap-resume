57e0c592fac14eacb5197df6e5c7999c6ce777fc
Venkateswara VP <venkateswara.vp@gmail.com>
2023-01-11 22:30:29 -0500

    Update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
